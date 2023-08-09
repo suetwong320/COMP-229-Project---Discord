ef8adedd1b55d96757b3fc69fb24ef938dcbfe3a
Leo <82315240+leohonlmc@users.noreply.github.com>
2023-08-09 19:50:27 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,49 @@
 var express = require("express");
 var router = express.Router();
+const session = require("express-session");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Discord" });
+  const loggedInUsername = req.session.loggedInUsername;
+
+  res.render("index", { title: "Discord", loggedInUsername });
 });
 
-/* GET home page. */
-router.get("/home", function (req, res, next) {
-  res.render("index", { title: "Home" });
+router.get("/logout", (req, res, next) => {
+  // Clear the session
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err); // Log the error for debugging
+      return res.status(500).json({ message: "An error occurred" });
+    }
+    // Redirect to the login page after logout
+    res.redirect("/login");
+  });
 });
 
-/* GET about page. */
-router.get("/about", function (req, res, next) {
-  res.render("index", { title: "About" });
-});
+// /* GET home page. */
+// router.get("/login", function (req, res, next) {
+//   res.render("login", { title: "Login" });
+// });
 
-/* GET contact page. */
-router.get("/contact", function (req, res, next) {
-  res.render("index", { title: "Contact" });
-});
+// /* GET about page. */
+// router.get("/register", function (req, res, next) {
+//   res.render("register", { title: "Register" });
+// });
 
-/* GET project page. */
-router.get("/project", function (req, res, next) {
-  res.render("index", { title: "Project" });
-});
+// /* GET contact page. */
+// router.get("/contact", function (req, res, next) {
+//   res.render("index", { title: "Contact" });
+// });
 
-/* GET service page. */
-router.get("/service", function (req, res, next) {
-  res.render("index", { title: "Service" });
-});
+// /* GET project page. */
+// router.get("/project", function (req, res, next) {
+//   res.render("index", { title: "Project" });
+// });
+
+// /* GET service page. */
+// router.get("/service", function (req, res, next) {
+//   res.render("index", { title: "Service" });
+// });
 
 module.exports = router;
